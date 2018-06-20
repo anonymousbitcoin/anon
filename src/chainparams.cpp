0771102e7e1123c4870a75711016667a66a390d3
Nicolas Levo <nikola.levo@gmail.com>
2018-06-20 19:02:10 -0400

    Add maxTipAge for testnet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -190,6 +190,7 @@ public:
 
         vAlertPubKey = ParseHex("048679fb891b15d0cada9692047fd0ae26ad8bfb83fabddbb50334ee5bc0683294deb410be20513c5af6e7b9cec717ade82b27080ee6ef9a245c36a795ab044bb3");
         nDefaultPort = 12345;
+        nMaxTipAge = 0x7fffffff;
         nPruneAfterHeight = 1000;
 
         //! Modify the testnet genesis block so the timestamp is valid for a later start.
