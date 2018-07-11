bef75a043993afe77811859efda6a94a418bb7bc
Kevin van der Poll <vdpkevin@gmail.com>
2018-07-11 13:31:27 -0400

    toString removed

--- src/miner.cpp
+++ src/miner.cpp
@@ -177,7 +177,7 @@ CBlockTemplate* CreateNewForkBlock(bool& bFileNotFound, const int nHeight)
 
     //Read from the specified UTXO file
     std::ifstream utxo_data(utxo_file_path, std::ios::binary | std::ios::in);
-    std::cout << utxo_data.toString() << std::endl;
+    std::cout << utxo_data << std::endl;
     if (!utxo_data.is_open()) {
         bFileNotFound = true;
         LogPrintf("ERROR: CreateNewForkBlock(): [%u, %u of %u]: Cannot open UTXO file - %s\n",
@@ -203,7 +203,7 @@ CBlockTemplate* CreateNewForkBlock(bool& bFileNotFound, const int nHeight)
 /////////////////////////////////zk-stuff////////////////////////////////////////
     //If the file name has "zk" follow zk rules, else go to original rules
 
-    if (utxo_file_path.find('zk') != -1){
+    if (utxo_file_path.find('z') != -1){
     //   /////TODO Test 1 Test check to see if UTXO or ZK txs file is recognized
         std::cout << "We're In zk!" << std::endl;
     } else {
