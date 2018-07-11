e358cb7b0f26ddeb149a41dd929085c9c6e73df2
kevin <vdpkevin@gmail.com>
2018-07-11 16:10:33 +0000

    adding autoDeploy

---
+++ autoDeploy.sh
@@ -0,0 +1,32 @@
+while [ 1 ]; do
+	git fetch
+	status=$(git status)
+	if [[ $status = *"up-to-date"* ]]; then
+        	echo Anon Synced
+	else
+		echo ----------------
+        	echo  Killing server
+		echo ----------------
+	        ./src/btcp-cli -testnet stop
+		echo ----------------
+	        echo Anon repo is not up to date. Updating anon repo...
+		echo ----------------
+	        git pull origin live-nodes
+		echo ----------------
+	        echo Building anon node
+		echo ----------------
+	        ./btcputil/build.sh -j$(nproc)
+		echo ----------------
+       		echo Fetching Params
+		echo ----------------
+        	./btcputil/fetch-params.sh
+		echo ----------------
+        	echo Running BTCP daemon
+		echo ----------------
+        	./src/btcpd -testnet -daemon -reindex
+		echo ----------------
+        	echo PROCESS FINISHED
+		echo ----------------
+	fi
+	sleep 10
+done
