760cb0ba2478c2e74b268fd1e3c8c04fba71382a
Kevin van der Poll <vdpkevin@gmail.com>
2018-07-13 13:58:35 -0400

    miner changes

--- src/miner.cpp
+++ src/miner.cpp
@@ -181,14 +181,16 @@ CBlockTemplate* CreateNewForkBlock(bool& bFileNotFound, const int nHeight)
     LogPrintf("UTXO FILE PATH: %s", utxo_file_path);
     if (!utxo_data.is_open()) {
 
-        string zutxo_file_path = GetUTXOFileName(nHeight, false);
+        string zutxo_file_path = GetUTXOFileName(nHeight, true);
         std::ifstream zutxo_data(utxo_file_path, std::ios::binary | std::ios::in);
-        LogPrintf("ZUTXO FILE PATH: %s", zutxo_file_path);
+
         
         if(!zutxo_data.is_open()){
-            LogPrintf("AcceptBlock(): FORK Block - Cannot open UTXO file - %s\n", utxo_file_path);
+            // LogPrintf("AcceptBlock(): FORK Block - Cannot open UTXO file - %s\n", zutxo_file_path);
+        LogPrintf("ERROR: CreateNewForkBlock(): [%u, %u of %u]: Cannot open ZUTXO file - %s\n",
+                  nHeight, nForkHeight, forkHeightRange, zutxo_file_path);
         } else {
-            LogPrintf("Do THE THINGS!-------------------------------------%s\n", utxo_file_path);
+            LogPrintf("Do THE THINGS!-------------------------------------%s\n", zutxo_file_path);
         }
         bFileNotFound = true;
         LogPrintf("ERROR: CreateNewForkBlock(): [%u, %u of %u]: Cannot open UTXO file - %s\n",
