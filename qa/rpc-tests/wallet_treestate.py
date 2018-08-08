83670d45b51dcf1fd2bfecdd12c3f755389b824d
ronin2 <ronin2>
2018-08-07 20:22:24 -0400

    wallet_1941.py, wallet_nullifiers.py, wallet_treestate.py tests now pass

--- qa/rpc-tests/wallet_treestate.py
+++ qa/rpc-tests/wallet_treestate.py
@@ -89,7 +89,7 @@ class WalletTreeStateTest (BitcoinTestFramework):
 
         # Tx 1 will change the treestate while Tx 2 containing chained joinsplits is still being generated
         recipients = []
-        recipients.append({"address":self.nodes[2].z_getnewaddress(), "amount":Decimal('10.0') - Decimal('0.0001')})
+        recipients.append({"address":self.nodes[2].z_getnewaddress(), "amount":Decimal('12.5') - Decimal('0.0001')})
         myopid = self.nodes[0].z_sendmany(mytaddr, recipients)
         self.wait_and_assert_operationid_status(myopid)
 
