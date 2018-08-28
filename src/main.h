e735db3c94bcaadba7a586b19db7f5d3691d1b12
Christopher Arguello <christopheraarguello@gmail.com>
2018-08-28 18:50:45 -0400

    Remove CScriptBase misuse in main.h

--- src/main.h
+++ src/main.h
@@ -541,7 +541,7 @@ struct CAddressUnspentValue {
     inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion)
     {
         READWRITE(satoshis);
-        READWRITE(*(CScriptBase*)(&script));
+        READWRITE(script);
         READWRITE(blockHeight);
     }
 
