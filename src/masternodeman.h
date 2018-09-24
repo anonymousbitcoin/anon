671e2d591fb384c4c3c42fe6d2d6e7fc842ee5f5
Nicolas Levo <nikola.levo@gmail.com>
2018-09-24 12:36:03 -0400

    WIP

--- src/masternodeman.h
+++ src/masternodeman.h
@@ -107,7 +107,7 @@ class CMasternodeMan
 
     static const int LAST_PAID_SCAN_BLOCKS = 100;
 
-    static const int MIN_POSE_PROTO_VERSION = 70203;
+    static const int MIN_POSE_PROTO_VERSION = 180003;
     static const int MAX_POSE_CONNECTIONS = 10;
     static const int MAX_POSE_RANK = 10;
     static const int MAX_POSE_BLOCKS = 10;
