06869bf6cb19324753dc575990c4468d41a508df
nlevo <nicolas.levo@gmail.com>
2019-03-18 13:17:01 -0400

    Fix reindex

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -838,7 +838,9 @@ void CWallet::DecrementNoteWitnesses(const CBlockIndex* pindex)
             }
         }
         // TODO: If nWitnessCache is zero, we need to regenerate the caches (#1302)
-        assert(nWitnessCacheSize > 0);
+        
+        if(pindex->nHeight > Params().ForkStartHeight() + Params().ForkHeightRange())
+            assert(nWitnessCacheSize > 0);
 
         // For performance reasons, we write out the witness cache in
         // CWallet::SetBestChain() (which also ensures that overall consistency
